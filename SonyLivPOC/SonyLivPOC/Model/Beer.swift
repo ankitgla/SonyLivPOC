c6d382963f3a688917c7b0edc32cc2bdfb13ca14
Ankit Bhardwaj <ankit.bhardwaj@timesinternet.in>
2021-09-22 15:27:04 +0530

    Complete data Modeling for parsing

--- SonyLivPOC/SonyLivPOC/Model/Beer.swift
+++ SonyLivPOC/SonyLivPOC/Model/Beer.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
+struct Quantity : Decodable {
+    var value:Decimal
+    var unit:String
+}
+
+struct MethodOperation : Decodable {
+    var temp:Quantity
+    var duration:Decimal?
+}
+
+struct Method :Decodable {
+    var mash_temp:[MethodOperation]
+    var fermentation:MethodOperation
+    var twist:String?
+}
+
+struct Ingredient : Decodable {
+    var name:String
+    var amount:Quantity
+    var add:String?
+    var attribute:String?
+}
+
+struct BeerIngredient : Decodable {
+    var malt:[Ingredient]
+    var hops:[Ingredient]
+    var yeast:String
+}
+
 struct Beer : Decodable {
     var id:Int
     var name:String
@@ -14,14 +43,19 @@ struct Beer : Decodable {
     var first_brewed:String
     var description:String
     var image_url:String
-    var abv:Float
-    var ibu:Float
+    var abv:Decimal
+    var ibu:Decimal
     var target_fg:Int
-    var target_og:Float
+    var target_og:Decimal
     var ebc:Int?
-    var srm:Float?
-    var ph:Float
-    var attenuation_level:Float
+    var srm:Decimal?
+    var ph:Decimal
+    var attenuation_level:Decimal
+    var volume:Quantity
+    var boil_volume:Quantity
+    var method:Method
+    var ingredients:BeerIngredient
+    var food_pairing:[String]
     var brewers_tips:String
     var contributed_by:String
 }
